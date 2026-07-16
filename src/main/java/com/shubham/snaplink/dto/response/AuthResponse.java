b77e3d6b693f88fb13c70582b7496facf6090593
Shubham shet <Shubhamshet204@gmail.com>
2026-07-16 10:12:25 +0530

    Added authentication DTOs

---
+++ src/main/java/com/shubham/snaplink/dto/response/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.shubham.snaplink.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+
+    private String message;
+}
